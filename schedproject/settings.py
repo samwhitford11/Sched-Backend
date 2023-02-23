935d68a654a299877ce7f2e54b61955e7e60e64d
samwhitford11 <samantha.fichte98@gmail.com>
2023-02-22 19:31:27 -0700

    deployment

--- schedproject/settings.py
+++ schedproject/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "schedules",
+    "schedules.apps.SchedulesConfig",
     'rest_framework',
     'corsheaders'
 ]
