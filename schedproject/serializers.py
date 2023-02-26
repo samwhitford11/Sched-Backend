801301d93244277ab07a08dc02a5a6fdb78618f7
samwhitford11 <samantha.fichte98@gmail.com>
2023-02-26 15:21:58 -0700

    adds to serializers

--- schedproject/serializers.py
+++ schedproject/serializers.py
@@ -2,10 +2,10 @@ from schedules.models import Sched
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
-# Our TodoSerializer
+# Our SChed Serializer
 class SchedSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         # The model it will serialize
         model = Sched
         # the fields that should be included in the serialized output
-        fields = ['id', 'title', 'date', 'time']
+        fields = ['id', 'title', 'date', 'time', 'location', 'notes']
